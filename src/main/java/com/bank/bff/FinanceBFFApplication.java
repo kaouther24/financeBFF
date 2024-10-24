0ed94643f9a0320bffd3235978ab4b2da85893be
KBE8168 <kaouther.ben.sassi@external.toyota-europe.com>
2024-10-24 11:03:14 +0200

    missed files

---
+++ src/main/java/com/bank/bff/FinanceBFFApplication.java
@@ -0,0 +1,13 @@
+package com.bank.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceBFFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceBFFApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/bff/FinanceBFFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceBFFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
